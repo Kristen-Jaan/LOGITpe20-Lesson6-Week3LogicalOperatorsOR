61b12730437d714f30767d40a9548d89c6842cbb
Opilane TTHK <opilane@tthk.ee>
2020-09-15 13:58:04 +0300

    first commit

---
+++ DaysOfTheWeek/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace DaysOfTheWeek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Good Morning!");
+            Console.WriteLine("What Day Is Today?");
+            string Day = Console.ReadLine().ToLower();
+            if(Day == "monday" || Day == "tuesday" || Day == "wednesday" || Day == "thursday" || Day == "friday")
+            {
+                Console.WriteLine($"{Day} Is A Workday.");
+            }
+            else if(Day == "saturday" || Day == "sunday")
+            {
+                Console.WriteLine("It's Weekend!");
+            }
+            else
+            {
+                Console.WriteLine("This Is Not A Day Of The Week, Debil.");
+            }
+
+        }
+    }
+}
---
+++ Week3LogicalOperatorsOR/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Week3LogicalOperatorsOR
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Good Morning!");
+            Console.WriteLine("What Month You Were Born?");
+            string Month = Console.ReadLine().ToLower();
+            if (Month == "june" || Month == "july" || Month == "august")
+
+            {
+                Console.WriteLine("You Were Born In Summer.");
+            }
+            else if (Month == "december" || Month == "january" || Month == "february")
+            {
+                Console.WriteLine("You Were Born In Winter.");
+            }
+            else if (Month == "march" || Month == "april" || Month == "may")
+            {
+                Console.WriteLine("You Were Born In Spring.");
+            }
+            else if (Month == "september" ||  Month == "october" || Month == "november" )
+            {
+                Console.WriteLine("You Were Born In Autumn.");
+            }
+            else
+            {
+                Console.WriteLine("This Is Not A Month, Debil.");
+            }
+
+        }
+    }
+}
